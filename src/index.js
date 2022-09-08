836ead3521cd8638af8880b28a67e733adf96f68
Your Name <ustinovoleksij@gmail.com>
2022-09-08 14:27:42 +0300

    HLEG-11 <Added a function>

--- src/index.js
+++ src/index.js
@@ -3,35 +3,28 @@ export const hotel = {
     priceByPlace: 20,
     bankAccount: 0,
     guests: {},
-    getLength: () => {
-        for (let key in hotel) {
-            console.log(this.guests);
-        }
+    getLength: function () {
+        return this.guests.length;
     },
-    getActualFreePlace: () => {
-        console.log(hotel.quantityOfPlaces - this.getLength)
+    getActualFreePlace: function () {
+        return this.quantityOfPlaces - this.getLength();
     },
-    paidPerPlace: () => {
-        return hotel.bankAccount + hotel.priceByPlace;
+    paidPerPlace: function () {
+        return this.bankAccount + this.priceByPlace;
     },
-    checkInGuest: (firstName, lastName, moneyQuantity) => {
-        /*Если в отеле нет свободных мест, то ничего не меняет внутри
-        объекта и возвращает строку 'Sorry, we have not free spaces'
-         */
-        if (this.getLength === 30) {
-            return 'Sorry, we do not have free spaces'
-            /*Если у пользователя не хватает денег(меньше нашего priceByPlace) то не меняем никак объект,
-            возвращаем строку 'Sorry, you have not enough money'*/
-        } else if (moneyQuantity < hotel.priceByPlace) {
-            return 'Sorry, you do not have enough money'
-            /*Если предыдущие условия не выполняются, мы добавляем в наши guests новый объект с данными(пример выше),
-            которые приняли в аргументы, но кроме количества денег. Вы должны отнять стоимость места, и остаток суммы
-             положить в объект.
-             */
+    addAGuest:function (firstName, lastName, moneyQuantity) {
+        const guestList = this.getLength(firstName, lastName, moneyQuantity);
+        moneyQuantity = moneyQuantity - this.priceByPlace;},
+    checkInGuest: function (firstName, lastName, moneyQuantity) {
+        if (this.guests.length >= this.quantityOfPlaces) {
+            return "Sorry, we do not have free spaces";
+        } else if (moneyQuantity < this.priceByPlace) {
+            return "Sorry, you do not have enough money"
         } else {
-            hotel.guests = () => `${firstName} ${lastName} ${moneyQuantity - hotel.priceByPlace}`
+          this.addAGuest();
+            }
         }
     }
-}
+
 
 
