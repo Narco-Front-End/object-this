15396a5d3ef950014b98c1f6d34c6750cef13438
Your Name <ustinovoleksij@gmail.com>
2022-09-27 13:02:09 +0300

    Added a function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-export const hotel = new Object();
+export const hotel = {
+    quantityOfPlaces: 30,
+    priceByPlace: 20,
+    bankAccount: 0,
+    guests: {},
+    getLength: function () {
+        return Object.keys(this.guests).length;
+    },
+    getActualFreePlace: function () {
+        return this.quantityOfPlaces - this.getLength();
+    },
+    paidByPlace: function () {
+        return this.bankAccount + this.priceByPlace;
+    },
+    checkInGuest: function (firstName, lastName, moneyQuantity) {
+        if (this.getLength() >= this.quantityOfPlaces) {
+            return 'Sorry, we do not have free spaces';
+        } else if (moneyQuantity < this.priceByPlace) {
+            return 'Sorry, you do not have enough money';
+        } else {
+            this.paidByPlace();
+        }
+    }
+};
+
+console.log(hotel.checkInGuest());
+console.log(hotel.bankAccount);
